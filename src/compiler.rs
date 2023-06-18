3256ed5e5fc232e313e4dda817fa6a90cd474a9f
Demelary <9827644+Firenezz@users.noreply.github.com>
2023-06-18 03:26:11 -0400

    Added support for comments / function / reordering

--- src/compiler.rs
+++ src/compiler.rs
@@ -8,12 +8,24 @@ pub mod lexer;
 pub struct LuoxidantParser;
 
 pub fn parse(source: &str) -> Result<(), pest::error::Error<Rule>> {
-    //let pairs = LuoxidantParser::parse(Rule::program, source)?;
-    /*for pair in pairs {
+    let pairs = LuoxidantParser::parse(Rule::lua, source)?;
+
+    let test = r#"
+        do
+            a = b
+            do
+                c = a
+            end
+            b = 5 * b
+            return (a + (c % 5))
+        end
+    "#;
+
+    for pair in pairs {
         println!("Rule:    {:?}", pair.as_rule());
         println!("Span:    {:?}", pair.as_span());
         println!("Text:    {}", pair.as_str());
         println!("Inner:   {:?}", pair.into_inner());
-    }*/
+    }
     Ok(())
 }
