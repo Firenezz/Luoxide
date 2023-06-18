bc16f7e54bcf437363df8681ed6f035e6d600db2
Demelary <9827644+Firenezz@users.noreply.github.com>
2023-06-18 01:44:58 -0400

    Expression worked

--- src/compiler.rs
+++ src/compiler.rs
@@ -8,12 +8,12 @@ pub mod lexer;
 pub struct LuoxidantParser;
 
 pub fn parse(source: &str) -> Result<(), pest::error::Error<Rule>> {
-    let pairs = LuoxidantParser::parse(Rule::program, source)?;
-    for pair in pairs {
+    //let pairs = LuoxidantParser::parse(Rule::program, source)?;
+    /*for pair in pairs {
         println!("Rule:    {:?}", pair.as_rule());
         println!("Span:    {:?}", pair.as_span());
         println!("Text:    {}", pair.as_str());
         println!("Inner:   {:?}", pair.into_inner());
-    }
+    }*/
     Ok(())
 }
